6cdd84f9861a7eba44d1e8f8c1a1dda3e3b2118a
Chi <mike.chi4@gmail.com>
2020-02-12 14:49:11 -0800

    initial intro to state using counters

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
-  console.log(navigation)
-
   return (
     <View>
       <Text style={styles.text}>Homepage</Text>
       <Button title="Go to Components Demo" onPress={() => {navigation.navigate('Components')}} />
       <Button title="Go to List Demo" onPress={() => {navigation.navigate('List')}} />
       <Button title="Go to Image Demo" onPress={() => {navigation.navigate('Image')}} />
+      <Button title="Go to Counter Demo" onPress={() => {navigation.navigate('Counter')}}/>
     </View>
   );
 };
